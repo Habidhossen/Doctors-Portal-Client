927c307b526d877534e48151bb1410d693030ee5
Habidhossen <habidhossen2@gmail.com>
2022-05-11 22:14:54 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
+import Navbar from "./pages/Shared/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
