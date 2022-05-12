6c701776c2559f375300d33e77ee414e472f6283
Habidhossen <habidhossen2@gmail.com>
2022-05-13 01:06:07 +0600

    service card added

---
+++ src/pages/Appointment/AppointmentCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PrimaryBtn from "../Shared/PrimaryBtn";
+
+const AppointmentCard = ({ service }) => {
+  const { name, slots } = service;
+
+  return (
+    <div class="card bg-base-100 shadow-xl">
+      <div class="card-body items-center text-center">
+        <h2 class="text-xl font-semibold text-secondary">{name}</h2>
+        <p className="text-sm font-normal text-black uppercase">
+          {slots.length ? (
+            slots[0]
+          ) : (
+            <span className="text-red-500">Try another date</span>
+          )}
+        </p>
+        <p className="text-xs font-normal text-black uppercase mb-3">
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+        </p>
+        <PrimaryBtn>Book Appointment</PrimaryBtn>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentCard;
--- src/pages/Appointment/AvailableAppointment.js
+++ src/pages/Appointment/AvailableAppointment.js
@@ -1,12 +1,27 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AppointmentCard from "./AppointmentCard";
 
 const AvailableAppointment = ({ date }) => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div>
-      <h1 className="text-center text-primary text-xl">
+      <h1 className="text-center text-secondary text-xl">
         Available Appointments on <b>{format(date, "PPP")}</b>
       </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-12 mt-16">
+        {services.map((service) => (
+          <AppointmentCard key={service._id} service={service} />
+        ))}
+      </div>
     </div>
   );
 };
